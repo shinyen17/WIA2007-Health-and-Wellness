6138e51786ad770f334754b741470e0513e6e738
linna <linna03152004@gmail.com>
2024-12-21 14:08:46 +0800

    first commit
    # Conflicts:
    #	app/build.gradle.kts
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/fragment_diet.xml
    #	app/src/main/res/layout/fragment_general.xml
    #	app/src/main/res/layout/fragment_mental.xml
    #	app/src/main/res/layout/fragment_past.xml
    #	app/src/main/res/layout/fragment_up_coming.xml
    #	app/src/main/res/values-night/themes.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/themes.xml
    #	gradle/wrapper/gradle-wrapper.properties
    #	settings.gradle.kts

--- app/src/main/java/com/example/combine/ConnectionClass.java
+++ app/src/main/java/com/example/combine/ConnectionClass.java
@@ -16,7 +16,7 @@ public class ConnectionClass {
 
     protected static String username = "root";
 
-    protected static String password = "password";
+    protected static String password = "host@123";
 
     public Connection CONN(){
         Connection conn = null;
